cd8d77faa5c6e00efd9ef3b5f4242da31021e0ee
pro-merndev <pro.merndev3@gmail.com>
2024-10-09 22:12:17 +0600

    Colors & Font Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#fffdfc",
+        foreground: "#000000",
+        grey: {
+          DEFAULT: "#F5F1F1",
+          main: "#F5F1F1",
+          dark: "#CBCBCB",
+          darker: "#707070",
+        },
+        primary: {
+          DEFAULT: "#271962",
+          lighter: "#F8F6FD",
+          main: "#271962",
+        },
+        secondary: {
+          DEFAULT: "#DE5515",
+          main: "#DE5515",
+        },
       },
     },
   },
