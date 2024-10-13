80c5727405d7268cbb69a3c8db1a028c68123fd2
pro-merndev <pro.merndev3@gmail.com>
2024-10-13 23:47:39 +0600

    applicaiton uncomment

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <HomeBanner />
       <HomeMarque />
       <HomeAdvantages />
-      {/* <HomeApplicationExample /> */}
+      <HomeApplicationExample />
       <HomeQuote />
       <HomeFeatures />
       <HomeSolutions />
