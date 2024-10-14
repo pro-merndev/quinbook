8373b577f0b667aa383e76aacf8f6286df9e462d
pro-merndev <pro.merndev3@gmail.com>
2024-10-15 00:26:01 +0600

    fix issue

--- src/components/shared/video-banner.tsx
+++ src/components/shared/video-banner.tsx
@@ -4,7 +4,7 @@ import { ClassValue } from "clsx";
 const VideoBanner = ({ src, className }: { src: string; className?: ClassValue }) => {
   return (
     <div className="container py-12 lg:py-20">
-      <video id="videoMoving" playsInline className={cn("rounded-3xl", className)} src={src} loop autoPlay muted />
+      <video id="videoMoving" playsInline className={cn("rounded-3xl w-full", className)} src={src} loop autoPlay muted />
     </div>
   );
 };
