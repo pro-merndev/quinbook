cb325a9892aa7d804ae201045cbc0abbe934b149
pro-merndev <pro.merndev3@gmail.com>
2024-10-15 21:48:53 +0600

    Parallax Effect Added to Home Banner

---
+++ src/components/shared/banner-image.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { motion, useScroll, useTransform } from "framer-motion";
+import { FC } from "react";
+
+type BannerImageProps = {
+  wrapperRef: React.RefObject<HTMLDivElement>;
+  imagePath: string;
+};
+
+const BannerImage: FC<BannerImageProps> = ({ wrapperRef, imagePath }) => {
+  const { scrollYProgress } = useScroll({
+    target: wrapperRef,
+    offset: ["start start", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], ["0%", "60%"]);
+
+  return (
+    <div className="pt-12 lg:pt-16 w-full h-auto aspect-[1080/636] relative rounded-[24px] overflow-hidden">
+      <motion.div
+        style={{
+          y,
+          backgroundImage: `url(${imagePath})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          borderRadius: "24px",
+        }}
+        className="absolute w-full h-full -top-[100px] -translate-y-1/2"
+      ></motion.div>
+    </div>
+  );
+};
+
+export default BannerImage;
