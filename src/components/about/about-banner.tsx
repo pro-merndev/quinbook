a9850fe81bd6033b7d0c475dfae033eda587b956
pro-merndev <pro.merndev3@gmail.com>
2024-10-15 21:57:31 +0600

    Parallax Banner Added to All Pages

--- src/components/about/about-banner.tsx
+++ src/components/about/about-banner.tsx
@@ -1,18 +1,20 @@
+"use client";
+
 import Banner from "@/components/shared/banner";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
+import { useRef } from "react";
+import BannerImage from "../shared/banner-image";
 import PrimaryButton from "../ui/primary-button";
 
 const AboutBanner = () => {
   const t = useTranslations("About.banner");
   const commonT = useTranslations("Common");
+  const ref = useRef(null);
 
   return (
-    <div className="container pb-16 lg:pb-[100px]">
+    <div className="container pb-16 lg:pb-[100px]" ref={ref}>
       <Banner title={t("title")} description={t("description")} button={<PrimaryButton>{commonT("learn_more")}</PrimaryButton>} />
-      <div className="pt-12 lg:pt-16 w-full h-auto aspect-[1080/636] relative">
-        <Image src="/images/about/banner.png" alt="Banner Image" fill className="object-cover rounded-[24px]" priority />
-      </div>
+      <BannerImage wrapperRef={ref} imagePath="/images/about/banner.png" />
     </div>
   );
 };
