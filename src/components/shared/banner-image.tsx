c7bbd9d4ab1f6d32dde0300a8f4a9ca01f952c2f
pro-merndev <pro.merndev3@gmail.com>
2024-10-16 11:09:18 +0600

    banner image component updated

--- src/components/shared/banner-image.tsx
+++ src/components/shared/banner-image.tsx
@@ -1,36 +1,33 @@
-"use client";
-
+import React, { forwardRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
-import { FC } from "react";
 
-type BannerImageProps = {
-  wrapperRef: React.RefObject<HTMLDivElement>;
-  imagePath: string;
-};
+const BannerImage = forwardRef<HTMLDivElement, { imagePath: string }>(
+  ({ imagePath }, wrapperRef) => {
+    const { scrollYProgress } = useScroll({
+      target: wrapperRef as any,
+      offset: ["start start", "end start"],
+    });
 
-const BannerImage: FC<BannerImageProps> = ({ wrapperRef, imagePath }) => {
-  const { scrollYProgress } = useScroll({
-    target: wrapperRef,
-    offset: ["start start", "end start"],
-  });
+    const y = useTransform(scrollYProgress, [0, 1], ["0%", "60%"]);
 
-  const y = useTransform(scrollYProgress, [0, 1], ["0%", "60%"]);
+    return (
+      <div className="pt-12 lg:pt-16 w-full h-auto aspect-[1080/636] relative rounded-[24px] overflow-hidden">
+        <motion.div
+          style={{
+            y,
+            backgroundImage: `url(${imagePath})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "24px",
+          }}
+          className="absolute w-full h-full -top-[100px]"
+        ></motion.div>
+      </div>
+    );
+  }
+);
 
-  return (
-    <div className="pt-12 lg:pt-16 w-full h-auto aspect-[1080/636] relative rounded-[24px] overflow-hidden">
-      <motion.div
-        style={{
-          y,
-          backgroundImage: `url(${imagePath})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          borderRadius: "24px",
-        }}
-        className="absolute w-full h-full -top-[100px]"
-      ></motion.div>
-    </div>
-  );
-};
+BannerImage.displayName = "BannerImage";
 
 export default BannerImage;
