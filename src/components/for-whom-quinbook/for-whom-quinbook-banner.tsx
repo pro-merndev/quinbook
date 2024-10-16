6e97978de0784497bb447b4a9753ef806be9b814
pro-merndev <pro.merndev3@gmail.com>
2024-10-16 22:52:11 +0600

    Build Error Fixed

--- src/components/for-whom-quinbook/for-whom-quinbook-banner.tsx
+++ src/components/for-whom-quinbook/for-whom-quinbook-banner.tsx
@@ -11,7 +11,7 @@ const ForWhomQuinbookBanner = () => {
   return (
     <div className="container pb-16 lg:pb-[100px]">
     <Banner title="FÜR WEN QUINBOOK?" description="Business Management einfach gemacht:  Optimiere deinen Workflow" button={<PrimaryButton contactButton>{commonT("learn_more")}</PrimaryButton>} />
-    <BannerImage wrapperRef={ref} imagePath="/images/features/feature_4_banner.jpeg" />
+    <BannerImage ref={ref} imagePath="/images/features/feature_4_banner.jpeg" />
   </div>
   );
 };
