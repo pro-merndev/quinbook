1ed7ce762e4edbd55e08571eab9bc1b9fb452e23
pro-merndev <pro.merndev3@gmail.com>
2024-10-27 00:09:57 +0600

    About Page Animation Done

--- src/components/about/about-team.tsx
+++ src/components/about/about-team.tsx
@@ -1,17 +1,22 @@
 import { TeamMembers } from "@/constants/team-members";
 import PrimaryBadge from "../ui/primary-badge";
 import TeamMemberCard from "../ui/team-member-card";
+import FadeIn from "../animation/fade-in";
 
 const AboutTeam = () => {
   return (
     <div id="team-member" className="container">
-      <h5 className="flex justify-center items-center">
-        <PrimaryBadge>Das team</PrimaryBadge>
-      </h5>
-      <h3 className="text-center font-poppins text-[42px] font-extrabold uppercase py-5">Über Uns</h3>
+      <FadeIn>
+        <h5 className="flex justify-center items-center">
+          <PrimaryBadge>Das team</PrimaryBadge>
+        </h5>
+        <h3 className="text-center font-poppins text-[42px] font-extrabold uppercase py-5">Über Uns</h3>
+      </FadeIn>
       <div className="mt-10 flex max-md:flex-col items-center justify-center gap-6">
         {TeamMembers.map((team, index) => (
-          <TeamMemberCard teamMember={team} key={index} />
+          <FadeIn key={index} delay={index * 0.2}>
+            <TeamMemberCard teamMember={team} />
+          </FadeIn>
         ))}
       </div>
     </div>
