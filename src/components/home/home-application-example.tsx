5dd849f2df67ce3d89b5bcb4d5926f2a193e597f
pro-merndev <pro.merndev3@gmail.com>
2024-11-05 00:29:33 +0600

    Home Animation Added Initially

--- src/components/home/home-application-example.tsx
+++ src/components/home/home-application-example.tsx
@@ -28,18 +28,20 @@ const HomeApplicationExample = () => {
             <div className="h-full">
               <div className="bg-primary md:py-40 py-10 md:px-12 px-5 max-lg:rounded-2xl">
                 {items.map((item, i) => (
-                  <div key={i} className="mb-9">
-                    <div className="flex gap-3 items-center ">
-                      <div className="bg-white rounded-xl p-5 w-full max-w-[600px] h-16">
-                        <p className="xl:text-2xl sm:text-xl text-sm">{item}</p>
-                      </div>
-                      <div>
-                        <span className="flex justify-center items-center p-3 bg-white rounded-full">
-                          <Image src="/logo/logo_icon.svg" alt="example-1" width={40} height={40} />
-                        </span>
+                  <FadeIn key={i}>
+                    <div className="mb-9">
+                      <div className="flex gap-3 items-center ">
+                        <div className="bg-white rounded-xl p-5 w-full max-w-[600px] h-16">
+                          <p className="xl:text-2xl sm:text-xl text-sm">{item}</p>
+                        </div>
+                        <div>
+                          <span className="flex justify-center items-center p-3 bg-white rounded-full">
+                            <Image src="/logo/logo_icon.svg" alt="example-1" width={40} height={40} />
+                          </span>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </FadeIn>
                 ))}
               </div>
             </div>
