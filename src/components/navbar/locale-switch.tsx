f14cf4a73c794aa6254977284101df409409075c
pro-merndev <pro.merndev3@gmail.com>
2024-11-09 11:08:22 +0600

    Refactor locale switch and Submenu Added to Navbar

--- src/components/navbar/locale-switch.tsx
+++ src/components/navbar/locale-switch.tsx
@@ -7,7 +7,6 @@ import { usePathname, useRouter } from "@/i18n/routing";
 import { cn } from "@/lib/utils";
 import { useLocale } from "next-intl";
 import Image from "next/image";
-import { useParams } from "next/navigation";
 import { useTransition } from "react";
 
 const LocaleSwitch = () => {
@@ -15,7 +14,6 @@ const LocaleSwitch = () => {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
-  const params = useParams();
 
   const handleChangeLanguage = (newLang: string) => {
     startTransition(() => {
@@ -25,29 +23,29 @@ const LocaleSwitch = () => {
   };
 
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
         <Button size="icon" variant="ghost" className="rounded-full p-1 size-8">
           <Image src={`/flags/${currentLocale}.svg`} alt={currentLocale} width={21} height={15} priority />
         </Button>
-      // </DropdownMenuTrigger>
-      // <DropdownMenuContent className="w-40">
-      //   <DropdownMenuLabel>Select Language</DropdownMenuLabel>
-      //   {config.localLanguages.map(({ label, value }) => (
-      //     <DropdownMenuItem
-      //       key={value}
-      //       onClick={() => handleChangeLanguage(value)}
-      //       className={cn(
-      //         value === currentLocale ? "bg-primary-lighter text-primary" : "text-foreground",
-      //         "font-semibold gap-2 cursor-pointer py-2"
-      //       )}
-      //     >
-      //       <Image src={`/flags/${value}.svg`} alt={currentLocale} width={21} height={15} priority />
-      //       <span>{label}</span>
-      //     </DropdownMenuItem>
-      //   ))}
-      // </DropdownMenuContent>
-    // </DropdownMenu>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40">
+        <DropdownMenuLabel>Select Language</DropdownMenuLabel>
+        {config.localLanguages.map(({ label, value }) => (
+          <DropdownMenuItem
+            key={value}
+            onClick={() => handleChangeLanguage(value)}
+            className={cn(
+              value === currentLocale ? "bg-primary-lighter text-primary" : "text-foreground",
+              "font-semibold gap-2 cursor-pointer py-2"
+            )}
+          >
+            <Image src={`/flags/${value}.svg`} alt={currentLocale} width={21} height={15} priority />
+            <span>{label}</span>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
--- src/components/navbar/navlinks.tsx
+++ src/components/navbar/navlinks.tsx
@@ -4,23 +4,58 @@ import { navLinks } from "@/constants/nav-links";
 import { Link, usePathname } from "@/i18n/routing";
 import clsx from "clsx";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
 
 const NavLinks = () => {
   const t = useTranslations();
   const featuresT = useTranslations("Features.items");
-  const pathname = usePathname();
 
   return (
     <div className="flex items-center gap-6">
       {navLinks(t, featuresT).map((link) => (
-        <Link
-          key={link.path}
-          href={link.path}
-          className={clsx(pathname === link.path ? "font-extrabold" : "hover:font-extrabold", "duration-200 transition-all")}
-        >
-          {link.label}
-        </Link>
+        <NavLinkItem key={link.path} link={link} />
       ))}
+    </div>
+  );
+};
+
+const NavLinkItem = ({ link }: { link: any }) => {
+  const pathname = usePathname();
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div key={link.path} className="relative" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <Link
+        key={link.path}
+        href={link.path}
+        className={clsx(pathname === link.path ? "font-extrabold" : "hover:font-extrabold", "duration-200 transition-all")}
+      >
+        {link.label}
+      </Link>
+
+      {link?.items && link?.items?.length && (
+        <div
+          className={clsx(
+            "absolute top-3 left-1/2 -translate-x-1/2 transform w-[270px] pt-12 transition-all duration-200 scale-95",
+            isHovered ? "scale-100 opacity-100" : "opacity-0 pointer-events-none"
+          )}
+        >
+          <div className="bg-primary-lighter rounded-[20px] overflow-hidden shadow-[0px_4px_9.2px_0px_#2719621A]">
+            {link.items.map((subItem: any) => (
+              <Link
+                key={subItem.path}
+                href={subItem.path}
+                className={clsx(
+                  "block py-4 px-6 duration-200 transition-all w-full",
+                  pathname === subItem.path ? "bg-primary/10 font-extrabold text-primary" : "hover:font-extrabold hover:bg-primary/10 hover:text-primary"
+                )}
+              >
+                {subItem.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
