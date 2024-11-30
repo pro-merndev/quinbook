bf117e33b8e08f0b0e2a6ec194f3baf9c763423b
pro-merndev <pro.merndev3@gmail.com>
2024-11-30 15:52:26 +0600

    Implement scroll-based visibility and animation for ForWhomQuinbook component

--- src/components/for-whom-quinbook/for-whom-quinbook-main.tsx
+++ src/components/for-whom-quinbook/for-whom-quinbook-main.tsx
@@ -1,34 +1,96 @@
-import React from 'react';
-import PrimaryBadge from '../ui/primary-badge';
-import { forWhomItems } from '@/constants/for-whom-items';
+"use client";
+
+/* eslint-disable @next/next/no-img-element */
+import { forWhomItems } from "@/constants/for-whom-items";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import PrimaryBadge from "../ui/primary-badge";
 
 const ForWhomQuinbookMain = () => {
+  const [visibleItemIndex, setVisibleItemIndex] = useState<number | null>(null);
+  const itemRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const index = itemRefs.current.indexOf(entry.target as HTMLDivElement);
+            setVisibleItemIndex(index);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    itemRefs.current.forEach((ref) => {
+      if (ref) {
+        observer.observe(ref);
+      }
+    });
+
+    return () => {
+      itemRefs.current.forEach((ref) => {
+        if (ref) {
+          observer.unobserve(ref);
+        }
+      });
+    };
+  }, []);
   return (
-    <div id="for-whom-quinbook">
-      <div  className="container py-10 lg:py-20">
-      <div className="flex flex-col gap-24">
+    <div id="for-whom-quinbook" className="container py-10 lg:py-20 flex gap-5">
+      <div className="flex flex-col max-lg:gap-12 w-full lg:w-[50%]">
         {forWhomItems.map((item, index) => (
-          <div key={index} className="md:grid md:grid-cols-2 md:gap-10 md:min-h-[750px]">
-            <div className="flex justify-center items-center gap-10 max-md:mb-10">
+          <div
+            key={index}
+            ref={(el) => {
+              itemRefs.current[index] = el;
+            }}
+            className="lg:min-h-screen"
+            style={{ scrollSnapAlign: "center" }}
+          >
+            <div className="flex flex-col justify-center items-start lg:items-center gap-4 h-full">
               <div>
                 <div>
-                  <PrimaryBadge>{item?.badge}</PrimaryBadge>
+                  <PrimaryBadge>{item.badge}</PrimaryBadge>
                 </div>
                 <h3 className="font-poppins font-extrabold md:leading-[90px] md:text-7xl text-4xl pt-5">{item?.title}</h3>
-                <p className='text-xl leading-7 text-grey-darker mt-4'>{item?.description_1}</p>
-                {item?.description_2 && <p className='text-xl leading-7 text-grey-darker mt-4'>{item?.description_2}</p>}
+                <p className="text-xl leading-7 text-grey-darker mt-4">{item?.description_1}</p>
+                {item?.description_2 && <p className="text-xl leading-7 text-grey-darker mt-4">{item?.description_2}</p>}
+              </div>
+              <div className="lg:hidden p-3 rounded-lg flex flex-col justify-center items-center gap-10 text-xl text-primary font-medium">
+                <img
+                  src={item?.image}
+                  alt="for whom quinbook"
+                  className="rounded-e-2xl h-full object-cover"
+                  style={{ aspectRatio: "16 / 9" }}
+                />
               </div>
             </div>
-            <img
-              src={item?.image}
-              alt="for whom quinbook"
-              className="rounded-e-2xl h-full object-cover"
-              style={{ aspectRatio: '16 / 9' }} // Adjust the aspect ratio as needed
-            />
           </div>
         ))}
       </div>
-    </div>
+
+      <div className="hidden lg:block sticky top-[7vh] right-0 h-[90vh] w-[50%] md:p-16 p-5 rounded-e-2xl">
+        <AnimatePresence>
+          {visibleItemIndex !== null && forWhomItems && (
+            <motion.div
+              key={visibleItemIndex}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2 }}
+              className="h-full"
+            >
+              <img
+                src={forWhomItems[visibleItemIndex]?.image}
+                alt="for whom quinbook"
+                className="rounded-e-2xl h-full object-cover"
+                style={{ aspectRatio: "16 / 9" }}
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
